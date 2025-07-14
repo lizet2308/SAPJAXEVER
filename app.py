d35dbbb7ff3e18ee87953358a853709f793ae0cd
Lucybell101 <119077531+Lucybell101@users.noreply.github.com>
2025-07-14 17:16:36 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,79 +1,46 @@
-from flask import Flask, render_template, request, redirect, url_for,  flash
-#Importamos el modulo de flask
-from flask_mysqldb import MySQL  # importamos modulo de mysql
+from flask import Flask, render_template, request, redirect, url_for, flash, Response, session
 
-app = Flask(__name__)  # Parametro
+app = Flask(__name__, template_folder='Templates')  # Parametro
 
-# CREAMOS VARIABLES DE CONEXION AL SERVIDOR DE MYSQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'mbdpy'
-
-mysql = MySQL(app)
-#Creamos Ruta Principal
+# Creamos Ruta Principal
 @app.route("/")
 def Index():
-    cu = mysql.connection.cursor()
-    cu.execute('select * from clientes')  # construimos la consulta
-    datos = cu.fetchall()  # ejecutamos para obtener todos los datos
-    # print(datos)  # imprime los datos de la consulta
-    return render_template('index.html', clients=datos)
-#Crear Ruta  Agregar Clientes
-@app.route("/add_clientes", methods=['POST'])  # Ruta de Acceso al archivo adicionar contacto
-def add_contact():
-    if request.method == 'POST':
-        cc = request.form['Cedula']
-        n = request.form['Nombres']
-        tel = request.form['Telefono']
-        em = request.form['Email']
-        # mbdpy = BASE DE DATOS
+    return render_template('suma.html')
 
-        cur = mysql.connection.cursor()  # EL CURSOR ME PERMITE EJECUTAR LAS CONSULTAS DE MYSQL
-        cur.execute('insert into clientes(id_cte,Nom_cte,Tel_cte,em_cte) values(%s,%s,%s,%s)',
-                    (cc, n, tel, em))
-        mysql.connection.commit()
-
-        # Confirmamos que se reciben los datos
-        print(f"C.c {cc} Nombres {n} Tel {tel} email {em}")
-        # return "Agregar Datos Recibidos" #RETORNA UN MENSAJE
-        return redirect(url_for('Index'))  # me redirecciona a la funcion de la ruta "/"
-#Crear Ruta  Consulta Dato Actualizar
-@app.route('/edit/<string:id>')  # Ruta de Acceso al archivo editar clientes
-def get_contact(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * from clientes WHERE id_cte={0}'.format(id))
-    dato = cur.fetchall()
-    return render_template ('Editacli.html' , clients=dato[0])
-#Crear Ruta  Consulta Dato Actualizar
-
-@app.route('/actualiza/<string:id>', methods=['POST'])  # Ruta de Acceso al archivo editar clientes
-def set_contact(id):
-
-    if request.method == 'POST':
-        cc = request.form['Cedula']
-        n = request.form['Nombres']
-        tel = request.form['Telefono']
-        em = request.form['Email']
-
-        cur = mysql.connection.cursor()
-        cur.execute("""
-            UPDATE clientes
-            SET id_cte=%s,
-                Nom_cte=%s,
-                Tel_cte=%s,
-                em_cte=%s
-            WHERE id_cte=%s
-        """, (cc, n, tel, em, id))
-
-        mysql.connection.commit()
-        return redirect(url_for('Index'))
-#Crear Ruta  Eliminar Clientes
-@app.route("/delete/<string:id>")  # Ruta de Acceso al archivo editar clientes
-def delete_contact(id):
-    cur = mysql.connection.cursor()
-    cur.execute('DELETE from clientes WHERE id_cte={0}'.format(id))
-    mysql.connection.commit()
-    return redirect(url_for('Index'))
+@app.route("/sumar", methods=["GET", "POST"])
+def OperSuma():
+    if request.method == "POST":
+        a = int(request.form["t1"])
+        b = int(request.form["t2"])
+        c = a + b
+        return render_template('suma.html', sm=c, pv=a, sv=b)
+    else:
+        return render_template('suma.html', sm=None)
+    
+@app.route("/imc", methods=["GET", "POST"])
+def OperIMC():
+    if request.method == "POST":
+        p = float(request.form["t1"])
+        alt = float(request.form["t2"])
+        i = p/(alt*alt)
+        
+        if i < 18.5:
+         estado = "Bajo peso"
+        elif i < 25:
+         estado = "Normal"
+        elif i < 30:
+         estado = "Sobrepeso"
+        elif i < 35:
+         estado = "Obesidad tipo 1"
+        elif i < 40:
+         estado = "Obesidad tipo 2"
+        else:
+         estado = "Obesidad tipo 3" 
+        
+        return render_template('imc.html', IM=i, pe=p, al=alt, es=estado)
+    else:
+        return render_template('imc.html', IM=None)
+    
+    
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
